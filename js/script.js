d9de7372d4dd81e90927ca4535baab9019b78cef
Edgardo Pinto-Escalier Scoffe <ep-marketing-media@live.com>
2016-05-23 21:22:38 +0200

    Added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,199 @@
-// Let's start with the search bar handler
+// This is the Searchbar Handler
 $(function(){
 	var searchField = $('#query');
 	var icon = $('#search-btn');
-
-// Next let's take care of the Focus Event Handler
+	
+	// Here we'll make the Focus Event Handler
 	$(searchField).on('focus', function(){
 		$(this).animate({
-			width: '100%'
+			width:'100%'
 		},400);
 		$(icon).animate({
 			right: '10px'
-		},400)
+		}, 400);
 	});
-
-// 	Here we will make the Blur Event Handler
+	
+	// This will be the Blur Event Handler
 	$(searchField).on('blur', function(){
-		if(searchField.val() == '') {
+		if(searchField.val() == ''){
 			$(searchField).animate({
-				width: '45%'
+				width:'45%'
 			},400, function(){});
 			$(icon).animate({
-				right: '360px'
+				right:'360px'
 			},400, function(){});
 		}
 	});
+	
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
 })
+
+
+function search(){
+	// Next we clear results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Here we get form input
+	q = $('#query').val();
+	
+	// Next we run GET request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyBqYVQdx0XCPxhy_laafli9ugrrL1tw7VU'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Here we log data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// And here we get Output
+					var output = getOutput(item);
+					
+					// Next we display results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// And here we display the buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Now we create the Next Page Function
+function nextPage(){
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+
+	// Here we clear results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Here we run GET request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video',
+			key: 'AIzaSyBqYVQdx0XCPxhy_laafli9ugrrL1tw7VU'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Here we log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// Next we get Output
+					var output = getOutput(item);
+					
+					// Now we display results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Next we display the buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+
+// Now we create the Prev Page Function
+function prevPage(){
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video',
+			key: 'AIzaSyBqYVQdx0XCPxhy_laafli9ugrrL1tw7VU'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Now we build Output
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+	
+	// Next we build Output string
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+	
+	return output;
+}
+
+// Finally we build the buttons
+function getButtons(prevPageToken, nextPageToken){
+	if(!prevPageToken){
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
+	
+	return btnoutput;
+}
